53084daf7a08cb292ab6188d6c465e5094d73c3e
lindont <coovoo@vip.qq.com>
2019-04-06 12:14:10 +0800

    HuangHaoXin

--- eureka-server/src/main/java/com/aos/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/aos/eureka/EurekaServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
+@SpringBootApplication
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
